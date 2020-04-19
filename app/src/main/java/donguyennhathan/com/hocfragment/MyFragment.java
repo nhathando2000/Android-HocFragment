3ae8dbd9d37165165b5844b5234be0d4585edc5d
handnn18411c@st.uel.edu.vn <handnn18411c@st.uel.edu.vn>
2020-04-19 14:32:15 +0700

    DoNguyenNhatHan commit "HocFragment"

---
+++ app/src/main/java/donguyennhathan/com/hocfragment/MyFragment.java
@@ -0,0 +1,30 @@
+package donguyennhathan.com.hocfragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, null);
+        Button btnOK = view.findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hello World!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+}
